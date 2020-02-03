7b127be4fd767f9e7abb2ee10402d281a57b792a
Prerna11071997 <writeprernasingh@gmail.com>
2020-02-03 15:19:41 +0530

    bot

--- app/src/main/java/com/prernasingh/bookethbot/MainActivity.java
+++ app/src/main/java/com/prernasingh/bookethbot/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 promptSpeechInput();
             }
         });
+
     }
 
     private void promptSpeechInput() {
@@ -194,6 +196,23 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
             tvOutput.setText(s);
 
+        }
+    }
+
+    private TextToSpeech mTts;
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                // success, create the TTS instance
+                mTts = new TextToSpeech(this, this);
+            } else {
+                // missing data, install it
+                Intent installIntent = new Intent();
+                installIntent.setAction(
+                        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+            }
         }
     }
 
